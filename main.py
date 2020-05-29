b143f42d6b0d8263f512158111ad529bd31a383a
ssbeast <ssbeast007@gmail.com>
2020-05-29 14:59:45 +0530

    player image displayed

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('img/spaceship.png')
 pygame.display.set_icon(icon)
 
+playerImg = pygame.image.load('img/player.png')
+playerX = 370
+playerY = 520
+
+def player():
+    screen.blit(playerImg,(playerX,playerY))
+
 running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    
+
+    player()
     pygame.display.update()
