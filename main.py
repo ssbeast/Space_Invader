b6aca0bab59e3f0cb51561043cc438a73258dcba
ssbeast <ssbeast007@gmail.com>
2020-07-14 06:36:44 +0530

    game over added

--- main.py
+++ main.py
@@ -48,12 +48,18 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
 def show_score(x,y):
     score = font.render("Score : " + str(score_value), True , (255,255,255))
     screen.blit(score, (x, y))
 
+def game_over_text(x, y):
+    over_text = over_font.render("GAME OVER", True , (255,255,255))
+    screen.blit(over_text, (200, 250))
+
 def player(x, y):
-    screen.blit(playerImg, (x,y))
+    screen.blit(playerImg, (x, y))
 
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x,y))
@@ -106,6 +112,13 @@ while running:
 
 
     for i in range(num_of_enemies): 
+
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0 :
             enemyX_change[i] = 1.5
